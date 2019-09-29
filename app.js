2b99a80fc1ab7da349ab1de32b72cdab8a655934
Slava <slava91petrushin@yandex.ru>
2019-09-29 12:00:34 +0300

    doubling of time

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+const http = require("http");
 
-app.get('/', require('./routes/index'))
+console.log('new Date().toUTCString() ' + new Date().toUTCString())
 
-const server = app.listen(process.env.PORT || 3000, () => {
-	console.log('Сервер запущен на порте: ' + server.address().port)
+let userInterval = process.argv[2];
+let userEnd = process.argv[3];
+
+if(typeof userInterval === 'string' &&  typeof userEnd === 'string'){
+	userInterval = Number(userInterval);
+	userEnd = Number(userEnd);
+} else {
+	userInterval = 5000;
+	userEnd = 60000;
+}
+
+http.createServer((req, res) => {
+	let now = new Date();
+	let nowStart = now.getTime();
+	let nowEnd = nowStart + userEnd;
+
+	let getDateConsole = setInterval(()=> {
+		let currentTime = new Date();
+		if(currentTime.getTime() <= nowEnd){
+			console.log(new Date().toUTCString());
+		} else {
+			clearInterval(getDateConsole);
+		}
+	}, userInterval);
+
+	res.writeHead(200, { 'Content': 'text/plain' })
+	res.end(now.toUTCString())
+}).listen(3000, () => {
+	console.log('Сервер работает')
 })
+
